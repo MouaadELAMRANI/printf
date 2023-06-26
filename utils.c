89c84be09c80c2406d9d19bf007bb829cf63fbfc
irmoad91 <i.moad33@gmail.com>
2023-06-26 14:38:17 -0700

    create print

--- utils.c
+++ utils.c
@@ -1,5 +1,22 @@
 #include "main.h"
 
+/*
+ * print - print a character
+ * @str: string
+ * Return: return to the length of a str
+ */
+
+int print(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		_putchar(str[x]);
+	}
+	return (x);
+}
+
 /**
  * _strlen - the length of a str
  * @str: string
