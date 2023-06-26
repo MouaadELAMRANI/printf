1cd7bc2f519e21b49bab3c36fee9e3ad7e11c75e
Mouaad <el.amrani.mouaad@gmail.com>
2023-06-26 13:06:05 -0700

    _formathandler

---
+++ _formathandler.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _formathandler - handles format
+ * @format: format to handle
+ * @vals: list of values
+ * Return: size
+ */
+int _formathandler(const char *format, va_list vals)
+{
+}
