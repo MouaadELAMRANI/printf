14fd2b32cbcbc84128936295baf461241f2cc9bc
Mouaad <el.amrani.mouaad@gmail.com>
2023-06-26 11:44:37 -0700

    initial draft for _printf

--- _printf.c
+++ _printf.c
@@ -7,5 +7,25 @@
  */
 int _printf(const char *format, ...)
 {
+	int size;
+	va_list args;
 
+	if (format == NULL)
+	{
+		return (-1);
+	}
+
+	size = _strlen(formation);
+	if (size <= 0)
+	{
+		return (0);
+	}
+
+	va_start(args, format);
+
+	handler(format, args);
+
+	va_end(args);
+
+	return (size);
 }
