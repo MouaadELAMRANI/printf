43676a70a25e2ce638d26833c409f39f47f8c4b6
Mouaad <el.amrani.mouaad@gmail.com>
2023-06-26 13:12:31 -0700

    _formathandler

--- _formathandler.c
+++ _formathandler.c
@@ -8,4 +8,22 @@
  */
 int _formathandler(const char *format, va_list vals)
 {
+	int size, i;
+
+	size = 0;
+	for (i = 0; str[i] != 0; i++)
+	{
+		if (str[i] == '%')
+		{
+			switch(str[i+1])
+			{
+				case 's':
+					//to do
+					break;
+				case 'c':
+					//to do
+					break;
+			}
+		}
+	}
 }
