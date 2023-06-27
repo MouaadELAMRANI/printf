67a20327366485c04792222ed61a1ca92519ba59
irmoad91 <i.moad33@gmail.com>
2023-06-27 11:17:24 -0700

    create function to print an integer

---
+++ print_int.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_int - print an integer in base 10
+ * @vals: list of number to print
+ * Return: return to the length of the numbers
+ */
+int print_int(va_list vals)
+{
+	char *ptr;
+	int length;
+
+	ptr = conv_into_str(va_arg(vals, int), 10);
+
+	length = print((ptr != NULL) ? ptr : "NULL");
+
+	return (length);
+}
